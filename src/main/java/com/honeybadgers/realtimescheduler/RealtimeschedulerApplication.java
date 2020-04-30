edcb19cb4b866feb882cff6f766b981d9c91db13
Felix Müller <felix.mueller0998@gmx.de>
2020-04-30 16:27:45 +0200

    created basic spring boot project

---
+++ src/main/java/com/honeybadgers/realtimescheduler/RealtimeschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.honeybadgers.realtimescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeschedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeschedulerApplication.class, args);
+    }
+
+}
