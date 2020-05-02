4114039c5c14f418f752c9eeace17bbee3509804
Felix Müller <felix.mueller0998@gmx.de>
2020-05-02 20:49:34 +0200

    added default config for building without db

--- src/main/java/com/honeybadgers/realtimescheduler/web/UserController.java
+++ src/main/java/com/honeybadgers/realtimescheduler/web/UserController.java
@@ -4,6 +4,7 @@ import com.honeybadgers.realtimescheduler.domain.User;
 import com.honeybadgers.realtimescheduler.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/user")
+@Profile({"redis", "postgre"})
 @Slf4j
 public class UserController {
 
