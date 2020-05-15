0daed2a78a93ad023e2d386adba97aee03ae7c45
Felix Müller <felix.mueller0998@gmx.de>
2020-05-15 15:40:24 +0200

    working parent-child pom setup @ChristoffSchaub

---
+++ application-apis/groupapi/src/main/java/com/honeybadgers/groupapi/controllers/DefaultApiController.java
@@ -0,0 +1,25 @@
+package com.honeybadgers.groupapi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-05-15T15:31:54.117+02:00[Europe/Berlin]")
+
+@Controller
+@RequestMapping("${openapi.Realtimescheduler Group Api.base-path:/api/group}")
+public class DefaultApiController implements DefaultApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public DefaultApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
---
+++ application-apis/monitoringapi/src/main/java/com/honeybadgers/monitoringapi/models/ErrorModel.java
@@ -0,0 +1,106 @@
+package com.honeybadgers.monitoringapi.models;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * ErrorModel
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-05-15T01:04:26.611+02:00[Europe/Berlin]")
+
+public class ErrorModel   {
+  @JsonProperty("code")
+  private String code;
+
+  @JsonProperty("errorMessage")
+  private String errorMessage;
+
+  public ErrorModel code(String code) {
+    this.code = code;
+    return this;
+  }
+
+  /**
+   * Get code
+   * @return code
+  */
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public ErrorModel errorMessage(String errorMessage) {
+    this.errorMessage = errorMessage;
+    return this;
+  }
+
+  /**
+   * Get errorMessage
+   * @return errorMessage
+  */
+  @ApiModelProperty(value = "")
+
+
+  public String getErrorMessage() {
+    return errorMessage;
+  }
+
+  public void setErrorMessage(String errorMessage) {
+    this.errorMessage = errorMessage;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ErrorModel errorModel = (ErrorModel) o;
+    return Objects.equals(this.code, errorModel.code) &&
+        Objects.equals(this.errorMessage, errorModel.errorMessage);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(code, errorMessage);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ErrorModel {\n");
+    
+    sb.append("    code: ").append(toIndentedString(code)).append("\n");
+    sb.append("    errorMessage: ").append(toIndentedString(errorMessage)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
