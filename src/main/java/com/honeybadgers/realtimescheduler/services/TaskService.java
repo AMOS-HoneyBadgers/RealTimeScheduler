ffb7a6732be00ce7bb25f91857fa126f09332bd1
MarcoDoell <marco.doell@hotmail.com>
2020-05-15 11:27:23 +0200

    added new findall methods for tasks and added new taskcontroller

--- src/main/java/com/honeybadgers/realtimescheduler/services/TaskService.java
+++ src/main/java/com/honeybadgers/realtimescheduler/services/TaskService.java
@@ -1,10 +1,13 @@
 package com.honeybadgers.realtimescheduler.services;
 
+import com.honeybadgers.realtimescheduler.model.Task;
 import com.honeybadgers.realtimescheduler.repository.TaskPostgresRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -12,4 +15,8 @@ public class TaskService {
 
     @Autowired
     TaskPostgresRepository taskPostgresRepository;
+
+    public List<Task> getAllTasks() {
+        return this.taskPostgresRepository.findAll();
+    }
 }
