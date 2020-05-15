f52936dd145414ad6c1a7ffbaae375d9f41406ad
MarcoDoell <marco.doell@hotmail.com>
2020-05-15 11:56:28 +0200

    delted openapi stuff / deleted redis examples

--- src/main/java/com/honeybadgers/realtimescheduler/RealtimeschedulerApplication.java
+++ src/main/java/com/honeybadgers/realtimescheduler/RealtimeschedulerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @Slf4j
-@EnableJpaRepositories
 public class RealtimeschedulerApplication {
 
     public static void main(String[] args) {
