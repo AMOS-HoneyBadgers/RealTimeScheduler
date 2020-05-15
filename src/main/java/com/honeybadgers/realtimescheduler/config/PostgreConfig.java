9da6ab5572f00585ae391beab3ccaae779bc5bf9
MarcoDoell <marco.doell@hotmail.com>
2020-05-15 11:56:28 +0200

    delted openapi stuff / deleted redis examples

--- src/main/java/com/honeybadgers/realtimescheduler/config/PostgreConfig.java
+++ src/main/java/com/honeybadgers/realtimescheduler/config/PostgreConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = {"com.honeybadgers.realtimescheduler.repository"})            // enable all jpa repositories in the given paths
-@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})// disable redis configuration
+@EnableJpaRepositories(basePackages = {"com.honeybadgers.realtimescheduler.repository"}) // enable all jpa repositories in the given paths
 public class PostgreConfig {
 }
