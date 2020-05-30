108979d868dde025a797d5aade42d18e466cd543
ChristoffSchaub <christoff.schaub@gmail.com>
2020-05-30 10:28:58 +0200

    implemented MockDispatcher in RabbitMQReceiver.java. Finished UserStory #46 and #42 with @MarcoDoell as Navigator and @ChristoffSchaub as Driver

--- application-main/src/main/java/com/honeybadgers/realtimescheduler/services/RabbitMQReceiver.java
+++ application-main/src/main/java/com/honeybadgers/realtimescheduler/services/RabbitMQReceiver.java
@@ -1,15 +1,32 @@
 package com.honeybadgers.realtimescheduler.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMQReceiver {
 
+    @Autowired
+    ICommunication communication;
+
     public void receiveTask(String in) {
         System.out.println(" [x] Received '" + in + "'");
+        try {
+            workTask();
+        }
+         catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        communication.sendFeedbackToScheduler("feedback sent");
+    }
+
+    private void workTask() throws InterruptedException {
+        long waitTime = (long) (Math.random() * ((10000 - 1000) + 1));
+        Thread.sleep( waitTime);
     }
 
     public void receiveFeedback(String in) {
         System.out.println(" [x] Received '" + in + "'");
+
     }
 }
