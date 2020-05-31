80c914adf134b8bba271d4c2c6d27a71ed3d463f
Felix Müller <felix.mueller0998@gmx.de>
2020-05-31 20:37:23 +0200

    fixed loadApplicationContext error in taskapi test

--- application-apis/taskapi/src/test/java/com/honeybadgers/taskapi/controllers/TaskIdApiControllerTest.java
+++ application-apis/taskapi/src/test/java/com/honeybadgers/taskapi/controllers/TaskIdApiControllerTest.java
@@ -1,21 +1,30 @@
 package com.honeybadgers.taskapi.controllers;
 
-
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import com.honeybadgers.taskapi.configuration.PostgreConfig;
+import com.honeybadgers.taskapi.repository.GroupRepository;
+import com.honeybadgers.taskapi.service.ITaskService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.persistence.EntityManagerFactory;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 import java.util.Collections;
@@ -37,6 +46,13 @@ public class TaskIdApiControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @TestConfiguration
+    @SpringBootApplication(exclude = {
+            PostgreConfig.class         // exclude PostgreConfig to prevent Jpa initialization
+    })
+    static class EmployeeServiceImplTestContextConfiguration {
+    }
 
     @Test
     public void taskIdGet() throws Exception {
