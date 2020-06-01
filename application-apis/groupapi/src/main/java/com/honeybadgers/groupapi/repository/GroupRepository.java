8ca2040d059ebad5ab01033d1e00ed617fb0e59f
MarcoDoell <marco.doell@hotmail.com>
2020-06-01 20:40:49 +0200

    added models in pom

--- application-apis/groupapi/src/main/java/com/honeybadgers/groupapi/repository/GroupRepository.java
+++ application-apis/groupapi/src/main/java/com/honeybadgers/groupapi/repository/GroupRepository.java
@@ -1,5 +1,6 @@
 package com.honeybadgers.groupapi.repository;
 
+
 import com.honeybadgers.models.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
