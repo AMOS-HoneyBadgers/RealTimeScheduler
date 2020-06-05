0eb8f267d4a2a6b5dd6143de780967d1c7dc35d7
Felix Müller <felix.mueller0998@gmx.de>
2020-06-05 13:55:02 +0200

    experimenting of readout of redis creds directly from injected envs instead of github secrets

--- application-main/src/main/java/com/honeybadgers/realtimescheduler/config/RedisConfig.java
+++ application-main/src/main/java/com/honeybadgers/realtimescheduler/config/RedisConfig.java
@@ -1,10 +1,11 @@
 package com.honeybadgers.realtimescheduler.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -14,24 +15,43 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories(basePackages = "com.honeybadgers.realtimescheduler")
 public class RedisConfig {
 
-    @Value("${spring.redis.prio.host}")
+    static final Logger logger = LogManager.getLogger(RedisConfig.class);
+
     private String priohost;
 
-    @Value("${spring.redis.prio.port}")
     private int prioport;
 
-    @Value("${spring.redis.prio.password}")
     private String priopassword;
 
-    @Value("${spring.redis.lock.host}")
     private String lockhost;
 
-    @Value("${spring.redis.lock.port}")
     private int lockport;
 
-    @Value("${spring.redis.lock.password}")
     private String lockpassword;
 
+    @Autowired
+    public RedisConfig(CloudFoundryProperties cloudConfig) {
+        logger.info("################################## START");
+        logger.warn("+++++++++++++++++++++ CLOUDCONFIG IS NULL: " + (cloudConfig == null));
+        logger.warn("++++++++++++++++++++ TEST TO STRING: " + cloudConfig.toString());
+        logger.warn("++++++++++++++++++++ prio_Store IS NULL: " + (cloudConfig.priority_store == null));
+        logger.warn("++++++++++++++++++++ lock_Store IS NULL: " + (cloudConfig.lock_store == null));
+        logger.warn("++++++++++++++++++++ prioStore creds: " + cloudConfig.priority_store.credentials.toString());
+        logger.warn("++++++++++++++++++++ lockStore creds: " + cloudConfig.lock_store.credentials.toString());
+        priohost = cloudConfig.priority_store.credentials.get("hostname");
+        logger.info("################################## host: " + priohost);
+        priopassword = cloudConfig.priority_store.credentials.get("password");
+        logger.info("################################## pw: " + priopassword);
+        prioport = Integer.parseInt(cloudConfig.priority_store.credentials.get("port"));
+        logger.info("################################## port: " + prioport);
+        lockhost = cloudConfig.lock_store.credentials.get("hostname");
+        logger.info("################################## host: " + lockhost);
+        lockpassword = cloudConfig.lock_store.credentials.get("password");
+        logger.info("################################## pw: " + lockpassword);
+        lockport = Integer.parseInt(cloudConfig.lock_store.credentials.get("port"));
+        logger.info("################################## port: " + lockport);
+        logger.info("################################## END");
+    }
 
     @Bean(name="prioConnectionFactory")
     JedisConnectionFactory jedisConnectionFactoryForPrioDatabase() {
@@ -66,9 +86,5 @@ public class RedisConfig {
         template.setConnectionFactory(jedisConnectionFactoryForLockDatabase());
         return template;
     }
-
-
-
-
 
 }
