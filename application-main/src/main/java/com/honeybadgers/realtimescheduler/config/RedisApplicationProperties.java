5d4ae477cf166c6be835d32ca1c0bcb04d499d26
Felix Müller <felix.mueller0998@gmx.de>
2020-06-05 21:36:25 +0200

    removed Redis application properties from github secrets and moved to user-provided from cloud foundry with @MarcoDoell and @LutskiYevgeniy

--- application-main/src/main/java/com/honeybadgers/realtimescheduler/config/RedisApplicationProperties.java
+++ application-main/src/main/java/com/honeybadgers/realtimescheduler/config/RedisApplicationProperties.java
@@ -10,22 +10,26 @@ import org.springframework.context.annotation.Configuration;
 @AllArgsConstructor
 @NoArgsConstructor
 @Configuration
-@ConfigurationProperties("spring.redis")
+@ConfigurationProperties("vcap.services.redis.credentials")
 public class RedisApplicationProperties {
 
-    RedisProperties prio;
+    String redis_lock_host;
+    String redis_lock_pw;
+    String redis_lock_port;
 
-    RedisProperties lock;
+    String redis_prio_host;
+    String redis_prio_pw;
+    String redis_prio_port;
 
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Data
-    public static class RedisProperties {
-
-        String host;
-
-        String password;
-
-        String port;
+    @Override
+    public String toString() {
+        return "RedisApplicationProperties{" +
+                "redis_lock_host='" + redis_lock_host + '\'' +
+                ", redis_lock_pw='" + redis_lock_pw + '\'' +
+                ", redis_lock_port='" + redis_lock_port + '\'' +
+                ", redis_prio_host='" + redis_prio_host + '\'' +
+                ", redis_prio_pw='" + redis_prio_pw + '\'' +
+                ", redis_prio_port='" + redis_prio_port + '\'' +
+                '}';
     }
 }
--- application-main/src/main/java/com/honeybadgers/realtimescheduler/config/RedisConfig.java
+++ application-main/src/main/java/com/honeybadgers/realtimescheduler/config/RedisConfig.java
@@ -15,44 +15,17 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories(basePackages = "com.honeybadgers.realtimescheduler")
 public class RedisConfig {
 
-    static final Logger logger = LogManager.getLogger(RedisConfig.class);
-
     @Autowired
     RedisApplicationProperties redisApplicationProperties;
 
-    public RedisConfig() {
-        /*logger.info("################################## START");
-        logger.warn("+++++++++++++++++++++ CLOUDCONFIG IS NULL: " + (cloudConfig == null));
-        logger.warn("++++++++++++++++++++ TEST TO STRING: " + cloudConfig.toString());
-        logger.warn("++++++++++++++++++++ prio_Store IS NULL: " + (cloudConfig.priority_store == null));
-        logger.warn("++++++++++++++++++++ lock_Store IS NULL: " + (cloudConfig.priority_store == null));
-        logger.warn("++++++++++++++++++++ prioStore creds: " + cloudConfig.priority_store.credentials.toString());
-        logger.warn("++++++++++++++++++++ lockStore creds: " + cloudConfig.priority_store.credentials.toString());
-        priohost = cloudConfig.priority_store.credentials.get("hostname");
-        logger.info("################################## host: " + priohost);
-        priopassword = cloudConfig.priority_store.credentials.get("password");
-        logger.info("################################## pw: " + priopassword);
-        prioport = Integer.parseInt(cloudConfig.priority_store.credentials.get("port"));
-        logger.info("################################## port: " + prioport);
-        lockhost = cloudConfig.priority_store.credentials.get("hostname");
-        logger.info("################################## host: " + lockhost);
-        lockpassword = cloudConfig.priority_store.credentials.get("password");
-        logger.info("################################## pw: " + lockpassword);
-        lockport = 15915;//Integer.parseInt(cloudConfig.priority_store.credentials.get("port"));
-        logger.info("################################## port: " + lockport);
-        logger.info("################################## END");*/
-    }
 
     @Bean(name="prioConnectionFactory")
     @Primary
     JedisConnectionFactory jedisConnectionFactoryForPrioDatabase() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        logger.warn("#################### PRIO DB HOST: " + redisApplicationProperties.prio.host);
-        logger.warn("#################### PRIO DB PW: " + redisApplicationProperties.prio.password);
-        logger.warn("#################### PRIO DB PORT: " + redisApplicationProperties.prio.port);
-        redisStandaloneConfiguration.setHostName(redisApplicationProperties.prio.host);
-        redisStandaloneConfiguration.setPassword(redisApplicationProperties.prio.password);
-        redisStandaloneConfiguration.setPort(Integer.parseInt(redisApplicationProperties.prio.port));
+        redisStandaloneConfiguration.setHostName(redisApplicationProperties.redis_prio_host);
+        redisStandaloneConfiguration.setPassword(redisApplicationProperties.redis_prio_pw);
+        redisStandaloneConfiguration.setPort(Integer.parseInt(redisApplicationProperties.redis_prio_port));
         return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
@@ -60,12 +33,9 @@ public class RedisConfig {
     @Bean(name="lockConnectionFactory")
     JedisConnectionFactory jedisConnectionFactoryForLockDatabase() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        logger.warn("#################### LOCK DB HOST: " + redisApplicationProperties.lock.host);
-        logger.warn("#################### LOCK DB PW: " + redisApplicationProperties.lock.password);
-        logger.warn("#################### LOCK DB PORT: " + redisApplicationProperties.lock.port);
-        redisStandaloneConfiguration.setHostName(redisApplicationProperties.lock.host);
-        redisStandaloneConfiguration.setPassword(redisApplicationProperties.lock.password);
-        redisStandaloneConfiguration.setPort(Integer.parseInt(redisApplicationProperties.lock.port));
+        redisStandaloneConfiguration.setHostName(redisApplicationProperties.redis_lock_host);
+        redisStandaloneConfiguration.setPassword(redisApplicationProperties.redis_lock_pw);
+        redisStandaloneConfiguration.setPort(Integer.parseInt(redisApplicationProperties.redis_lock_port));
         return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
