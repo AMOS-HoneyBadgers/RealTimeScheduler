5167c8f209b3db541119035b275594327694a233
MarcoDoell <marco.doell@hotmail.com>
2020-06-06 19:51:09 +0200

    test for management service with @f3l1x98 and @LutskiYevgeniy

--- application-apis/managementapi/src/test/java/com/honeybadgers/managementapi/service/ManagementServiceTest.java
+++ application-apis/managementapi/src/test/java/com/honeybadgers/managementapi/service/ManagementServiceTest.java
@@ -1,18 +1,26 @@
 package com.honeybadgers.managementapi.service;
 
 
+import com.honeybadgers.managementapi.repository.StateRepository;
 import com.honeybadgers.managementapi.service.impl.ManagementService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = ManagementService.class)
 public class ManagementServiceTest {
+    @MockBean
+    StateRepository stateRepository;
 
+    @Autowired
+    ManagementService service;
     @Test
     public void pauseScheduler() {
+
     }
 
     @Test
