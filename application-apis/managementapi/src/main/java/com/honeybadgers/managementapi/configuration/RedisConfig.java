0b7acce0fd726969322d83fb4c7c29f952a0cbc7
MarcoDoell <marco.doell@hotmail.com>
2020-06-06 19:12:46 +0200

    added redis configs in management api with @f3l1x98

---
+++ application-apis/managementapi/src/main/java/com/honeybadgers/managementapi/configuration/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.honeybadgers.managementapi.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories(basePackages = "com.honeybadgers.managementapi")
+public class RedisConfig {
+
+    @Autowired
+    RedisApplicationProperties redisApplicationProperties;
+
+    @Bean(name="lockConnectionFactory")
+    JedisConnectionFactory jedisConnectionFactoryForLockDatabase() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisApplicationProperties.redis_lock_host);
+        redisStandaloneConfiguration.setPassword(redisApplicationProperties.redis_lock_pw);
+        redisStandaloneConfiguration.setPort(Integer.parseInt(redisApplicationProperties.redis_lock_port));
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean(name="lockRedisTemplate")
+    public RedisTemplate<String, Object> lockRedisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactoryForLockDatabase());
+        return template;
+    }
+
+}
--- application-apis/managementapi/src/main/java/com/honeybadgers/managementapi/repository/StateRepository.java
+++ application-apis/managementapi/src/main/java/com/honeybadgers/managementapi/repository/StateRepository.java
@@ -7,60 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public class StateRepository implements CrudRepository<RedisTask, String> {
+public interface StateRepository extends CrudRepository<String, String> {
 
-    @Override
-    public <S extends RedisTask> S save(S s) {
-        return null;
-    }
-
-    @Override
-    public <S extends RedisTask> Iterable<S> saveAll(Iterable<S> iterable) {
-        return null;
-    }
-
-    @Override
-    public Optional<RedisTask> findById(String s) {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean existsById(String s) {
-        return false;
-    }
-
-    @Override
-    public Iterable<RedisTask> findAll() {
-        return null;
-    }
-
-    @Override
-    public Iterable<RedisTask> findAllById(Iterable<String> iterable) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(String s) {
-
-    }
-
-    @Override
-    public void delete(RedisTask redisTask) {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends RedisTask> iterable) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
 }
