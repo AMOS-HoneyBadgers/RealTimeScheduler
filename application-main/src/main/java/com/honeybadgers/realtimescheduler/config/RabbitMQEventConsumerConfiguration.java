8abb3785083a91277eba553609dc5ade1e3389ba
ChristoffSchaub <christoff.schaub@gmail.com>
2020-06-06 11:10:23 +0200

    RabbitMQEventConsumerConfiguration.java refactored
    RabbitMQEventConsumerConfiguration.java refactored
    
    RabbitMQEventConsumerConfiguration.java refactored

--- application-main/src/main/java/com/honeybadgers/realtimescheduler/config/RabbitMQEventConsumerConfiguration.java
+++ application-main/src/main/java/com/honeybadgers/realtimescheduler/config/RabbitMQEventConsumerConfiguration.java
@@ -1,27 +1,26 @@
 package com.honeybadgers.realtimescheduler.config;
 
+import com.honeybadgers.communication.config.RabbitMQApplicationProperties;
 import com.honeybadgers.realtimescheduler.services.TaskConsumerRabbit;
 import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//TODO Intellij shows that the class is not able to autowire rabbitMQApplicationProperties
+//TODO but there are no test failures and the scheduler seems to do the work
+//TODO check together what
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @Configuration
 public class RabbitMQEventConsumerConfiguration {
 
-    @Value("${dispatch.rabbitmq.tasksqueue}")
-    private String taskqueue;
-
-    @Value("${dispatch.rabbitmq.tasksroutingkey}")
-    private String tasksroutingkey;
-
-    @Value("${dispatch.rabbitmq.tasksexchange}")
-    String tasksExchange;
+    @Autowired
+    RabbitMQApplicationProperties rabbitMQApplicationProperties;
 
     @Bean
     public Declarables topicBindings() {
-        Queue queue = new Queue(taskqueue);
-        DirectExchange topicExchange = new DirectExchange(tasksExchange);
+        Queue queue = new Queue(rabbitMQApplicationProperties.getTasksqueue());
+        DirectExchange topicExchange = new DirectExchange(rabbitMQApplicationProperties.getTasksexchange());
 
         return new Declarables(
                 queue,
@@ -29,7 +28,7 @@ public class RabbitMQEventConsumerConfiguration {
                 BindingBuilder
                 .bind(queue)
                 .to(topicExchange)
-                .with(tasksroutingkey)
+                .with(rabbitMQApplicationProperties.getTasksroutingkey())
         );
     }
 }
