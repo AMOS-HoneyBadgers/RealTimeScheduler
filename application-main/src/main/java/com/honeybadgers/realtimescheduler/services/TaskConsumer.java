bd148f00eda8519f5331d6a40dfa38e5525297ce
MarcoDoell <marco.doell@hotmail.com>
2020-06-07 20:20:51 +0200

    merge

--- application-main/src/main/java/com/honeybadgers/realtimescheduler/services/TaskConsumer.java
+++ application-main/src/main/java/com/honeybadgers/realtimescheduler/services/TaskConsumer.java
@@ -1,17 +1,24 @@
 package com.honeybadgers.realtimescheduler.services;
 
 import com.honeybadgers.communication.model.TaskQueueModel;
+import com.honeybadgers.realtimescheduler.services.impl.SchedulerService;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableRabbit
 public class TaskConsumer {
 
+    @Autowired
+    SchedulerService service;
+
     @RabbitListener(queues="tasks", containerFactory = "taskcontainerfactory")
     public void receiveTask(String message) {
         System.out.println("Received message '{}'" + message);
+        System.out.println("### Starting with Step:1 Scheduling Task");
+        service.scheduleTask(message.toString());
     }
 
     @RabbitListener(queues="priority", containerFactory = "priorityContainerFactory")
