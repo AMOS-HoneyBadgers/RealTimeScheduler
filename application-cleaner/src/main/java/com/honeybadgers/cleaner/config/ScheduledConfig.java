950c58fb74f9a716754ace14007859fd0a963714
Felix Müller <felix.mueller0998@gmx.de>
2020-06-14 17:49:45 +0200

    disabled jpa auto config with @MarcoDoell

--- application-cleaner/src/main/java/com/honeybadgers/cleaner/config/ScheduledConfig.java
+++ application-cleaner/src/main/java/com/honeybadgers/cleaner/config/ScheduledConfig.java
@@ -1,9 +1,18 @@
 package com.honeybadgers.cleaner.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class ScheduledConfig {
 }
