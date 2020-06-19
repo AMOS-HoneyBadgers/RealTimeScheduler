3ea5e84334ada9623e77f1080d6d06f025407365
Pascal Vahldiek <pascal.vahldiek@fau.de>
2020-06-19 12:47:04 +0200

    Added model classes and post method for testing performance with @MarcoDoell

--- application-clienttests/src/main/java/com/honeybadgers/clienttests/performance/PerformanceService.java
+++ application-clienttests/src/main/java/com/honeybadgers/clienttests/performance/PerformanceService.java
@@ -1,8 +1,14 @@
 package com.honeybadgers.clienttests.performance;
 
+import com.honeybadgers.clienttests.models.ResponseModel;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PerformanceService {
@@ -16,5 +22,35 @@ public class PerformanceService {
     public String getPostsPlainJSON() {
         String url = "https://jsonplaceholder.typicode.com/posts";
         return this.restTemplate.getForObject(url, String.class);
+    }
+
+    public ResponseModel createPost() {
+        String url = "https://jsonplaceholder.typicode.com/posts";
+
+        // create headers
+        HttpHeaders headers = new HttpHeaders();
+        // set `content-type` header
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        // set `accept` header
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        // create a map for post parameters
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", 1);
+        map.put("title", "Introduction to Spring Boot");
+        map.put("body", "Spring Boot makes it easy to create stand-alone, production-grade Spring based Applications.");
+
+        // build the request
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+
+        // send POST request
+        ResponseEntity<ResponseModel> response = this.restTemplate.postForEntity(url, entity, ResponseModel.class);
+
+        // check response status code
+        if (response.getStatusCode() == HttpStatus.CREATED) {
+            return response.getBody();
+        } else {
+            return null;
+        }
     }
 }
