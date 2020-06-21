440d7451ac6ed34c28b2fc634aee6d76edd797b9
ji15wymo <lyegeniy@googlemail.com>
2020-06-21 14:41:04 +0200

    Calender_day of week and converutils from RTS mismatch

--- application-main/src/test/java/com/honeybadgers/realtimescheduler/services/impl/ConvertUtilsTest.java
+++ application-main/src/test/java/com/honeybadgers/realtimescheduler/services/impl/ConvertUtilsTest.java
@@ -9,14 +9,13 @@ class ConvertUtilsTest {
     void fitDayOfWeekToWorkingDayBools() {
         ConvertUtils convertUtils = new ConvertUtils();
         //Sonntag
-        Assert.assertEquals(0,convertUtils.fitDayOfWeekToWorkingDayBooleans(1));
+        Assert.assertEquals(6,convertUtils.fitDayOfWeekToWorkingDayBooleans(1));
         //Montag
-        Assert.assertEquals(1,convertUtils.fitDayOfWeekToWorkingDayBooleans(2));
-        Assert.assertEquals(2,convertUtils.fitDayOfWeekToWorkingDayBooleans(3));
-        Assert.assertEquals(3,convertUtils.fitDayOfWeekToWorkingDayBooleans(4));
-        Assert.assertEquals(4,convertUtils.fitDayOfWeekToWorkingDayBooleans(5));
-        Assert.assertEquals(5,convertUtils.fitDayOfWeekToWorkingDayBooleans(6));
-        Assert.assertEquals(6,convertUtils.fitDayOfWeekToWorkingDayBooleans(0));
+        Assert.assertEquals(0,convertUtils.fitDayOfWeekToWorkingDayBooleans(2));
+        Assert.assertEquals(1,convertUtils.fitDayOfWeekToWorkingDayBooleans(3));
+        Assert.assertEquals(2,convertUtils.fitDayOfWeekToWorkingDayBooleans(4));
+        Assert.assertEquals(3,convertUtils.fitDayOfWeekToWorkingDayBooleans(5));
+        Assert.assertEquals(4,convertUtils.fitDayOfWeekToWorkingDayBooleans(6));
+        Assert.assertEquals(5,convertUtils.fitDayOfWeekToWorkingDayBooleans(7));
     }
-
 }
