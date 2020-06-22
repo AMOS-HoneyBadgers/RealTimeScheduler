a35bff858c883ccaf3850a57029ecfefc4afaf28
Pascal Vahldiek <pascal.vahldiek@fau.de>
2020-06-22 21:01:02 +0200

    Added Timestamps for performance measure with @MarcoDoell

--- application-clienttests/src/main/java/com/honeybadgers/clienttests/performance/PerformanceController.java
+++ application-clienttests/src/main/java/com/honeybadgers/clienttests/performance/PerformanceController.java
@@ -14,7 +14,13 @@ public class PerformanceController {
 
     @GetMapping("/performance/{count}")
     public ResponseEntity<?> createPerformanceTest(@PathVariable(value = "count") final int count) {
+        long before = System.currentTimeMillis();
+        System.out.println("Before: " + before);
         performanceService.createPostWithObject(count);
+        long after = System.currentTimeMillis();
+        System.out.println("After: " + after);
+
+        System.out.println("Diff: " + (after - before));
         return ResponseEntity.ok().build();
     }
 }
