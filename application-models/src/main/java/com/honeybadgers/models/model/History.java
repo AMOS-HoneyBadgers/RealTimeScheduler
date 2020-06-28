32b30427d537241a14e4a71686e38febcd78dad2
ji15wymo <lyegeniy@googlemail.com>
2020-06-28 15:45:06 +0200

    removed an unused import @MarcoDoell

--- application-models/src/main/java/com/honeybadgers/models/model/History.java
+++ application-models/src/main/java/com/honeybadgers/models/model/History.java
@@ -1,11 +1,9 @@
 package com.honeybadgers.models.model;
 
-import jdk.jfr.Timespan;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Time;
 import java.sql.Timestamp;
 
 @Data
@@ -16,4 +14,5 @@ public class History {
     private String status;
 
     private Timestamp timestamp;
+
 }
