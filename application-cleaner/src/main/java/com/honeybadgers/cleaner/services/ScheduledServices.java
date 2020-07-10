f37af767a4232d0b0bbc6b24122b19a5f2f59063
Felix Müller <felix.mueller0998@gmx.de>
2020-07-10 11:59:03 +0200

    new scheduled method calling the deleteAllTasksFinishedSinceNMilliseconds once a day (configurable via properties) IF ENABLED (also configurable via properties)

--- application-cleaner/src/main/java/com/honeybadgers/cleaner/services/ScheduledServices.java
+++ application-cleaner/src/main/java/com/honeybadgers/cleaner/services/ScheduledServices.java
@@ -2,7 +2,9 @@ package com.honeybadgers.cleaner.services;
 
 import com.honeybadgers.communication.ICommunication;
 import com.honeybadgers.models.model.Paused;
+import com.honeybadgers.models.model.Task;
 import com.honeybadgers.postgre.repository.PausedRepository;
+import com.honeybadgers.postgre.repository.TaskRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,10 +27,19 @@ public class ScheduledServices {
     PausedRepository pausedRepository;
 
     @Autowired
+    TaskRepository taskRepository;
+
+    @Autowired
     ICommunication sender;
 
     @Value("${scheduler.trigger}")
     String scheduler_trigger;
+
+    @Value("${cleaner.finished-cleanup.enabled}")
+    boolean taskCleanupEnabled;
+
+    @Value("${cleaner.finished-cleanup.expiration-days}")
+    int taskCleanupDays;
 
     /**
      * This Component check for paused Tasks and Groups and makes sure they are unlocked at specified times.
@@ -41,6 +52,20 @@ public class ScheduledServices {
         if(deleted.size() > 0) {
             sender.sendTaskToTasksQueue(scheduler_trigger);
             logger.info("Notified scheduler for rescheduling!");
+        }
+    }
+
+
+    @Scheduled(fixedRateString = "${cleaner.finished-cleanup.fixed-rate}", initialDelayString = "${cleaner.finished-cleanup.initial-delay}")
+    public void cleanFinishedTasks() {
+        if(taskCleanupEnabled) {
+            logger.info("Cleaner starting finished tasks cleanup");
+            // convert number of days to milliseconds
+            long daysInMillis = taskCleanupDays * 24 * 60 * 60 * 1000L;
+            List<Task> deleted = taskRepository.deleteAllTasksFinishedSinceNMilliseconds(daysInMillis);
+            logger.info("Cleaner finished task cleanup - deleted " + deleted.size() + " tasks, which were finished for at least " + taskCleanupDays + " days.");
+        } else {
+            logger.info("Automated task deletion is disabled - please enable in application.properties");
         }
     }
 }
